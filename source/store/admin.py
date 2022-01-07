494449318fc7d2478933216ea0fb87691cfb164a
Aaimry <wheal28@gmail.com>
2022-01-07 20:45:09 +0600

    создала модель Productsб зарегистрировала в админке и сделала миграцию

--- source/store/admin.py
+++ source/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Products
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'price']
+    list_filter = ['title', 'category']
+    search_fields = ['title', 'category']
+    fields = ['title', 'description', 'category', 'residue', 'price']
+
+
+admin.site.register(Products, ProductsAdmin)
--- source/store/models.py
+++ source/store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [('other', 'Разное'), ('fruits', 'Фрукты'), ('vegetables', 'Овощи'), ('beverage', 'Напитки'),
+                    ('bakery', 'Выпечка')]
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=15, default='other', verbose_name='Категория')
+    residue = models.IntegerField(null=False, verbose_name='Остаток')
+    price = models.FloatField(max_length=7, null=False, verbose_name='Цена')
+
+
+def __str__(self):
+    return f'{self.title} {self.description} {self.category} {self.residue} {self.price}'
+
+
+class Meta:
+    db_table = 'Products'
+    verbose_name = 'Продукт'
+    verbose_name_plural = 'Продукты'
